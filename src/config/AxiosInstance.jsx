6d61820edc20f92c1b62a23812b635b9d52445c8
Duban Estiben Cardozo Osorio <60492285+Minirick@users.noreply.github.com>
2024-09-19 02:20:18 -0500

    axios 2

--- src/config/AxiosInstance.jsx
+++ src/config/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://3.17.27.249:8000', // Usa la variable de entorno
+  baseURL: 'https://3.17.27.249:8000', // Usa la variable de entorno
   // baseURL: import.meta.env.VITE_API_BASE_URL, // Usa la variable de entorno
   headers: {
     'Content-Type': 'application/json',
