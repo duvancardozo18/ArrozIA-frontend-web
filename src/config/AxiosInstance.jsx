4675df0eafc4144b6a5ac497e5f86fb7de593f04
Duban Estiben Cardozo Osorio <60492285+Minirick@users.noreply.github.com>
2024-09-19 14:08:39 -0500

    fix: entorno virtual

--- src/config/AxiosInstance.jsx
+++ src/config/AxiosInstance.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://3.17.27.249:8000', // Usa la variable de entorno
-  // baseURL: import.meta.env.VITE_API_BASE_URL, // Usa la variable de entorno
+  baseURL: import.meta.env.VITE_API_BASE_URL, // Usa la variable de entorno
   headers: {
     'Content-Type': 'application/json',
   },
