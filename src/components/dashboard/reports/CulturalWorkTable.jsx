ecd9e91f66eeb66124ef0c14fad61814602d4b99
Duban Estiben Cardozo Osorio <60492285+Minirick@users.noreply.github.com>
2024-11-21 16:38:25 -0500

    fix: importacion archivo CropInputsTable en Cultural Work

--- src/components/dashboard/reports/CulturalWorkTable.jsx
+++ src/components/dashboard/reports/CulturalWorkTable.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axiosInstance from "../../../config/AxiosInstance";
-import "../../../css/cropinputstable.scss";
+import "../../../css/CropInputsTable.scss";
 
 const CulturalWorkTable = ({ cultivoId, onFilteredDataChange }) => {
   const [culturalWorks, setCulturalWorks] = useState([]);
